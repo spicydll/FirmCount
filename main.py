23c798a9e60f55c829c3995a26b51eed3ad21695
spicydll <spicydll@github.com>
2021-01-30 17:40:30 +0100

    Improve main structure MS

--- main.py
+++ main.py
@@ -1,9 +1,14 @@
 import binwalk
+import sys
+
+def doExtraction(file):
+    for module in binwalk.scan(file, signature=True, quiet=True, extract=True):
+        print ("%s Results:" % module.name)
+        for result in module.results:
+            print ("\t%s    0x%.8X    %s" % (result.file.path, result.offset, result.description))
 
 def main():
-    binwalk.scan()
-    for module in binwalk.scan('firmware/MERGE_IOT_V22.bin',signature=True,quiet=False, extract=True):
-        print ("%s Results:" % module.name)
+    doExtraction(sys.argv[1])
 
 if __name__ == "__main__":
     main()
