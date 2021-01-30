f8c0f6613263cb5e2470da2e2e9b11d28b5d5620
spicydll <spicydll@github.com>
2021-01-30 18:09:16 +0100

    Add run_test.sh to assist dev MS

--- main.py
+++ main.py
@@ -2,7 +2,7 @@ import binwalk
 import sys
 
 def doExtraction(file):
-    for module in binwalk.scan(file, quiet=True, extract=True, signature=True, directory="out/" + file + "/"):
+    for module in binwalk.scan(file, quiet=True, extract=True, signature=True, directory="out/"):
         print ("%s Results:" % module.name)
         for result in module.results:
             print ("\t%s    0x%.8X    %s" % (result.file.path, result.offset, result.description))
---
+++ run_test.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+
+rm -rf ./out/*
+
+python main.py firmware/MERGE_IOT_V22.bin
