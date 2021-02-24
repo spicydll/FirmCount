fadafb2a036b80d3835602c2cf027194fcb41000
spicydll <spicydll@github.com>
2021-02-24 11:05:52 +0100

    doing more stuff nice

--- main.py
+++ main.py
@@ -17,15 +17,18 @@ def scanForFunc(file):
     r.cmd('aa')
     file_info = r.cmdj('ij')
     funcs = r.cmdj('aflj')
-    
+
     if file_info['core']['format'] == 'elf':
-        print('Is elf lol')
-
-
+       print('Is elf lol')
+       for f in funcs:
+           print(f)
+#        for f in funcs:
+#            print(f['name'])
+#            print(f) 
 
 def main():
     doExtraction(sys.argv[1])
-    scanForFunc('out/_c7v5_[20201120-rel50406]_2020-11-20_14.00.44.bin.extracted/squashfs-root/usr/bin/ledctrl')
+    scanForFunc('out/_c7v5_[20201120-rel50406]_2020-11-20_14.00.44.bin.extracted/squashfs-root/usr/bin/ledcli')
   #  scanForFunc()
 
 if __name__ == "__main__":
