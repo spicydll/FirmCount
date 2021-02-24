4483bc56eddac463cf7660cce959e6a080c09028
spicydll <spicydll@github.com>
2021-02-24 10:36:16 +0100

    Getting somewhere now

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 import binwalk
 import r2pipe
 import sys
+import logging
 
 def doExtraction(file):
     for module in binwalk.scan(file, quiet=True, extract=True, signature=True, directory="out/"):
@@ -8,13 +9,23 @@ def doExtraction(file):
         for result in module.results:
             print ("\t%s    0x%.8X    %s" % (result.file.path, result.offset, result.description))
 
-
-def scanForFunc(file, functions):
-    pass
+# functions: list of functions to check for
+def scanForFunc(file):
+    #testing: scan for printf
+    print('R2 Scan:')
+    r = r2pipe.open(file)
+    r.cmd('aa')
+    file_info = r.cmdj('ij')
+    funcs = r.cmdj('aflj')
     
+    if file_info['core']['format'] == 'elf':
+        print('Is elf lol')
+
+
 
 def main():
     doExtraction(sys.argv[1])
+    scanForFunc('out/_c7v5_[20201120-rel50406]_2020-11-20_14.00.44.bin.extracted/squashfs-root/usr/bin/ledctrl')
   #  scanForFunc()
 
 if __name__ == "__main__":
