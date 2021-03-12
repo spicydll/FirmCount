91dd94a4da2e26a6450918e993145a07f7f5858a
spicydll <spicydll@github.com>
2021-03-12 23:55:09 +0100

    add more stuff MS

--- database.py
+++ database.py
@@ -1,52 +1,85 @@
 import sqlite3
 
-CREATE_SCRIPT = '''
-CREATE TABLE Manufacturers (
-	id integer PRIMARY KEY AUTOINCREMENT,
-	name varchar
-);
+class iotDB:
 
-CREATE TABLE Images (
-	id blob,
-	man_id blob,
-	release_date datetime,
-	name varchar
-);
+    def __init__(self):
+        self.database = 'iotcount.db'
+        self.con = sqlite3.connect(self.database)
+        self.cur = self.con.cursor()
 
-CREATE TABLE Files (
-	id blob
-);
+    def create(self):
+        CREATE_SCRIPT = '''
+        CREATE TABLE Manufacturers (
+            id integer PRIMARY KEY AUTOINCREMENT,
+            name varchar
+        );
 
-CREATE TABLE Functions (
-	id integer PRIMARY KEY AUTOINCREMENT,
-	name varchar,
-	vuln_desc varchar
-);
+        CREATE TABLE Images (
+            id blob,
+            man_id blob,
+            release_date datetime,
+            name varchar
+        );
 
-CREATE TABLE Detections (
-	func_id integer,
-	file_id blob,
-	call_ref blob
-);
+        CREATE TABLE Files (
+            id blob
+        );
 
-CREATE TABLE ImageFiles (
-	file_id blob,
-	image_id blob,
-	name varchar
-);
-'''
+        CREATE TABLE Functions (
+            id integer PRIMARY KEY AUTOINCREMENT,
+            name varchar,
+            vuln_desc varchar
+        );
 
-DROP_SCRIPT = '''
-DROP TABLE IF EXISTS Manufacturers;
+        CREATE TABLE Detections (
+            func_id integer,
+            file_id blob,
+            call_ref blob
+        );
 
-DROP TABLE IF EXISTS Images;
+        CREATE TABLE ImageFiles (
+            file_id blob,
+            image_id blob,
+            name varchar
+        );
+        '''
 
-DROP TABLE IF EXISTS Files;
+        self.cur.executescript(CREATE_SCRIPT)
+        self.con.commit()
 
-DROP TABLE IF EXISTS Functions;
+    def destroy(self):
+        DROP_SCRIPT = '''
+        DROP TABLE IF EXISTS Manufacturers;
 
-DROP TABLE IF EXISTS Detections;
+        DROP TABLE IF EXISTS Images;
 
-DROP TABLE IF EXISTS ImageFiles;
-'''
+        DROP TABLE IF EXISTS Files;
+
+        DROP TABLE IF EXISTS Functions;
+
+        DROP TABLE IF EXISTS Detections;
+
+        DROP TABLE IF EXISTS ImageFiles;
+        '''
+
+        self.cur.executescript(DROP_SCRIPT)
+        self.con.commit()
+
+    
+    def reinit(self):
+        self.destroy()
+        self.create()
+
+    def getManufacturer(self, name):
+        check_man = 'SELECT id FROM Manufacturers WHERE name = ?'
+        self.cur.execute(check_man, name)
+        row = self.cur.fetchone()
+
+        if (row is not None):
+            return row['id']
+        else:
+            new_man = 'INSERT INTO Manufacturers (name) VALUES (?)'
+            self.cur.execute(new_man, name)
+            self.cur.commit()
+            return cur.lastrowid
 
